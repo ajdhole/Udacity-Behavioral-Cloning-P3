b083bb047f92724d797c0b35464eaa5c8f02d1b1
Anil Dhole <ajdhole@gmail.com>
2017-07-30 14:22:08 +0530

    Track_1 Training run

--- model.py
+++ model.py
@@ -1,190 +1,146 @@
-import tensorflow as tf
+import os
+import csv
+import cv2
 import numpy as np
 import random
-import csv
-import cv2 
-import json
-import h5py
-
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Dropout, Cropping2D
+import sklearn
 from sklearn.utils import shuffle
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+
+
+
 from sklearn.model_selection import train_test_split
-from keras.layers import Activation, Dense, Dropout, ELU, Flatten, Input, Lambda
-from keras.layers.convolutional import Convolution2D, Cropping2D
-from keras.models import Sequential, Model, load_model, model_from_json
-from keras.regularizers import l2
-
-
-def get_csv_data(log_file):
-    """
-    Reads a csv file and returns two lists separated into examples and labels.
-    :param log_file: The path of the log file to be read.
-    """
-    image_names, steering_angles = [], []
-    # Steering offset used for left and right images
-    steering_offset = 0.275
-    with open(log_file, 'r') as f:
-        reader = csv.reader(f)
-        next(reader, None)
-        for center_img, left_img, right_img, angle, _, _, _ in reader:
-            angle = float(angle)
-            image_names.append([center_img.strip(), left_img.strip(), right_img.strip()])
-            steering_angles.append([angle, angle+steering_offset, angle-steering_offset])
-
-    return image_names, steering_angles
-
-
-def generate_batch(X_train, y_train, batch_size=64):
-    """
-    Return two numpy arrays containing images and their associated steering angles.
-    :param X_train: A list of image names to be read in from data directory.
-    :param y_train: A list of steering angles associated with each image.
-    :param batch_size: The size of the numpy arrays to be return on each pass.
-    """
-    images = np.zeros((batch_size, 66, 200, 3), dtype=np.float32)
-    angles = np.zeros((batch_size,), dtype=np.float32)
-    while True:
-        straight_count = 0
-        for i in range(batch_size):
-            # Select a random index to use for data sample
-            sample_index = random.randrange(len(X_train))
-            image_index = random.randrange(len(X_train[0]))
-            angle = y_train[sample_index][image_index]
-            # Limit angles of less than absolute value of .1 to no more than 1/2 of data
-            # to reduce bias of car driving straight
-            if abs(angle) < .1:
-                straight_count += 1
-            if straight_count > (batch_size * .5):
-                while abs(y_train[sample_index][image_index]) < .1:
-                    sample_index = random.randrange(len(X_train))
-            # Read image in from directory, process, and convert to numpy array
-            image = cv2.imread('data/' + str(X_train[sample_index][image_index]))
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image = process_image(image)
-            image = np.array(image, dtype=np.float32)
-            # Flip image and apply opposite angle 50% of the time
-            if random.randrange(2) == 1:
-                image = cv2.flip(image, 1)
-                angle = -angle
-            images[i] = image
-            angles[i] = angle
-        yield images, angles
-
-  """  for sample in samples:
-        angle = abs(float(sample[3]))
-        if angle < del_angle:
-            if np.random.random() < del_rate:
-                continue"""
-def resize(image):
-    """
-    Returns an image resized to match the input size of the network.
-    :param image: Image represented as a numpy array.
-    """
-    return cv2.resize(image, (200, 66), interpolation=cv2.INTER_AREA)
-
-
-def normalize(image):
-    """
-    Returns a normalized image with feature values from -1.0 to 1.0.
-    :param image: Image represented as a numpy array.
-    """
-    return image / 127.5 - 1.
-
-
-def crop_image(image):
-    """
-    Returns an image cropped 40 pixels from top and 20 pixels from bottom.
-    :param image: Image represented as a numpy array.
-    """
-    return image[40:-20,:]
 
 
 def random_brightness(image):
     """
-    Returns an image with a random degree of brightness.
-    :param image: Image represented as a numpy array.
+    Randomly adjust brightness of the image.
     """
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    brightness = .25 + np.random.uniform()
-    image[:,:,2] = image[:,:,2] * brightness
-    image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+    # HSV (Hue, Saturation, Value) is also called HSB ('B' for Brightness).
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    ratio = 1.0 + 0.4 * (np.random.rand() - 0.5)
+    hsv[:,:,2] =  hsv[:,:,2] * ratio
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+def add_random_shadow(image):
+    top_y = image.shape[1]*np.random.uniform()
+    top_x = 0
+    bot_x = image.shape[0]
+    bot_y = image.shape[1]*np.random.uniform()
+    image_hls = cv2.cvtColor(image,cv2.COLOR_RGB2HLS)
+    shadow_mask = 0*image_hls[:,:,1]
+    X_m = np.mgrid[0:image.shape[0],0:image.shape[1]][0]
+    Y_m = np.mgrid[0:image.shape[0],0:image.shape[1]][1]
+    
+    shadow_mask[((X_m-top_x)*(bot_y-top_y) -(bot_x - top_x)*(Y_m-top_y) >=0)]=1
+    random_bright = .15+.8*np.random.uniform()
+    if np.random.randint(2)==1:
+        #    random_bright = .5
+        cond1 = shadow_mask==1
+        cond0 = shadow_mask==0
+        if np.random.randint(2)==1:
+            image_hls[:,:,1][cond1] = image_hls[:,:,1][cond1]*random_bright
+        else:
+            image_hls[:,:,1][cond0] = image_hls[:,:,1][cond0]*random_bright
+    image = cv2.cvtColor(image_hls,cv2.COLOR_HLS2RGB)
     return image
 
 
-def process_image(image):
-    """
-    Returns an image after applying several preprocessing functions.
-    :param image: Image represented as a numpy array.
-    """
-    image = random_brightness(image)
-    image = crop_image(image)
-    image = resize(image)
-    return image
+def get_driving_log(path):
+    lines = []
+    with open(path + 'driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+            
+    return lines
+
+del_angle = 0.01
+del_rate = 0.85
+
+def generator(path, samples, batch_size=128):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        samples = sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for image_nb in [0, 1, 2]:
+                    name = path + 'IMG/'+ batch_sample[image_nb].split('/')[-1]
+                    image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_BGR2RGB)
+                    image_b = random_brightness(image)
+                    shift_dict = {0: 0, 1: 0.3, 2: -0.3}
+                    angle = float(batch_sample[3]) + shift_dict.get(image_nb, "error")
+                    if angle < del_angle: # to ignore zero steering angle data
+                        if np.random.random() < del_rate:
+                            continue
+                
 
 
-def get_model():
-    """
-    Returns a compiled keras model ready for training.
-    """
-    model = Sequential([
-        # Normalize image to -1.0 to 1.0
-        Lambda(normalize, input_shape=(66, 200, 3)),
-        # Convolutional layer 1 24@31x98 | 5x5 kernel | 2x2 stride | elu activation 
-        Convolution2D(24, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with drop probability of .1 (keep probability of .9)
-        Dropout(.1),
-        # Convolutional layer 2 36@14x47 | 5x5 kernel | 2x2 stride | elu activation
-        Convolution2D(36, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with drop probability of .2 (keep probability of .8)
-        Dropout(.2),
-        # Convolutional layer 3 48@5x22  | 5x5 kernel | 2x2 stride | elu activation
-        Convolution2D(48, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with drop probability of .2 (keep probability of .8)
-        Dropout(.2),
-        # Convolutional layer 4 64@3x20  | 3x3 kernel | 1x1 stride | elu activation
-        Convolution2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with drop probability of .2 (keep probability of .8)
-        Dropout(.2),
-        # Convolutional layer 5 64@1x18  | 3x3 kernel | 1x1 stride | elu activation
-        Convolution2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='he_normal', W_regularizer=l2(0.001)),
-        # Flatten
-        Flatten(),
-        # Dropout with drop probability of .3 (keep probability of .7)
-        Dropout(.3),
-        # Fully-connected layer 1 | 100 neurons | elu activation
-        Dense(100, activation='elu', init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with drop probability of .5
-        Dropout(.5),
-        # Fully-connected layer 2 | 50 neurons | elu activation
-        Dense(50, activation='elu', init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with drop probability of .5
-        Dropout(.5),
-        # Fully-connected layer 3 | 10 neurons | elu activation
-        Dense(10, activation='elu', init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with drop probability of .5
-        Dropout(.5),
-        # Output
-        Dense(1, activation='linear', init='he_normal')
-    ])
+                    images.append(np.fliplr(image_b))
+                    angles.append(-angle)
+                
+                    images.append(image_b)
+                    angles.append(angle)
 
-    model.compile(optimizer='adam', loss='mse')
-    return model    
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+# ------ Training/Validation data loading -------
+
+path = './data/'
+
+samples = get_driving_log(path)
+
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+# compile and train the model using the generator function
+train_generator = generator(path, train_samples, batch_size=32)
+validation_generator = generator(path, validation_samples, batch_size=32)
+
+row, col, ch =160, 320, 3  # Trimmed image format
+BATCH_SIZE = 32
+
+def resize_im(x):
+    from keras.backend import tf
+    return tf.image.resize_images(x, (80, 160))
+
+model = Sequential()
+model.add(Cropping2D(cropping=((40,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 127.5 - 1))
+model.add(Lambda(resize_im))
+
+model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(64, 3, 3, activation='elu'))
+model.add(Conv2D(64, 3, 3, activation='elu'))
+model.add(Dropout(0.1))
+model.add(Flatten())
+model.add(Dense(100, activation='elu'))
+model.add(Dense(50, activation='elu'))
+model.add(Dense(10, activation='elu'))
+model.add(Dense(1))
+model.summary()
+
+checkpoint = ModelCheckpoint('model-{epoch:03d}.h5', monitor='val_loss', verbose=0, save_best_only='true' , mode='auto')
+
+model.compile(loss='mse', optimizer='adam')
 
 
-if __name__=="__main__":
+model.fit_generator(train_generator, samples_per_epoch = 10000, validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5, callbacks=[checkpoint], verbose=1)
+model.save('model.h5')
 
-    # Get the training data from log file, shuffle, and split into train/validation datasets
-    X_train, y_train = get_csv_data('data/driving_log.csv')
-    X_train, y_train = shuffle(X_train, y_train, random_state=14)
-    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.1, random_state=14)
-
-    # Get model, print summary, and train using a generator
-    model = get_model()
-    model.summary()
-    model.fit_generator(generate_batch(X_train, y_train), samples_per_epoch=24000, nb_epoch=28, validation_data=generate_batch(X_validation, y_validation), nb_val_samples=1024)#, callbacks=[early_stop])
-
-    print('Saving model weights and configuration file.')
-    # Save model weights
-    model.save_weights('model.h5')
-    # Save model architecture as json file
-    with open('model.json', 'w') as outfile:
-        json.dump(model.to_json(), outfile)
+# Explicitly end tensorflow session
+from keras import backend as K
+    
+K.clear_session()
