bb379c50ee7f8652744f0f75107e366a55c23484
Anil Dhole <ajdhole@gmail.com>
2017-08-06 20:40:51 +0530

    Final Submission

--- model.py
+++ model.py
@@ -3,15 +3,15 @@ import csv
 import cv2
 import numpy as np
 import random
+import math
+
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, Dropout, Cropping2D
 import sklearn
 from sklearn.utils import shuffle
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
-
-
-
+from keras.layers.normalization import BatchNormalization
 from sklearn.model_selection import train_test_split
 
 
@@ -20,34 +20,10 @@ def random_brightness(image):
     Randomly adjust brightness of the image.
     """
     # HSV (Hue, Saturation, Value) is also called HSB ('B' for Brightness).
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
     ratio = 1.0 + 0.4 * (np.random.rand() - 0.5)
     hsv[:,:,2] =  hsv[:,:,2] * ratio
-    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-
-def add_random_shadow(image):
-    top_y = image.shape[1]*np.random.uniform()
-    top_x = 0
-    bot_x = image.shape[0]
-    bot_y = image.shape[1]*np.random.uniform()
-    image_hls = cv2.cvtColor(image,cv2.COLOR_RGB2HLS)
-    shadow_mask = 0*image_hls[:,:,1]
-    X_m = np.mgrid[0:image.shape[0],0:image.shape[1]][0]
-    Y_m = np.mgrid[0:image.shape[0],0:image.shape[1]][1]
-    
-    shadow_mask[((X_m-top_x)*(bot_y-top_y) -(bot_x - top_x)*(Y_m-top_y) >=0)]=1
-    random_bright = .15+.8*np.random.uniform()
-    if np.random.randint(2)==1:
-        #    random_bright = .5
-        cond1 = shadow_mask==1
-        cond0 = shadow_mask==0
-        if np.random.randint(2)==1:
-            image_hls[:,:,1][cond1] = image_hls[:,:,1][cond1]*random_bright
-        else:
-            image_hls[:,:,1][cond0] = image_hls[:,:,1][cond0]*random_bright
-    image = cv2.cvtColor(image_hls,cv2.COLOR_HLS2RGB)
-    return image
-
+    return cv2.cvtColor(hsv, cv2.COLOR_HLS2RGB)
 
 def get_driving_log(path):
     lines = []
@@ -58,10 +34,10 @@ def get_driving_log(path):
             
     return lines
 
-del_angle = 0.01
-del_rate = 0.85
+del_angle = 0.001
+del_rate = 0.5
 
-def generator(path, samples, batch_size=128):
+def generator(path, samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         samples = sklearn.utils.shuffle(samples)
@@ -74,19 +50,17 @@ def generator(path, samples, batch_size=128):
                 for image_nb in [0, 1, 2]:
                     name = path + 'IMG/'+ batch_sample[image_nb].split('/')[-1]
                     image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_BGR2RGB)
-                    image_b = random_brightness(image)
-                    shift_dict = {0: 0, 1: 0.3, 2: -0.3}
+                    image = random_brightness(image)
+                    shift_dict = {0: 0, 1: 0.27, 2: -0.27}
                     angle = float(batch_sample[3]) + shift_dict.get(image_nb, "error")
                     if angle < del_angle: # to ignore zero steering angle data
                         if np.random.random() < del_rate:
                             continue
-                
 
-
-                    images.append(np.fliplr(image_b))
+                    images.append(np.fliplr(image))
                     angles.append(-angle)
                 
-                    images.append(image_b)
+                    images.append(image)
                     angles.append(angle)
 
             # trim image to only see section with road
@@ -97,7 +71,7 @@ def generator(path, samples, batch_size=128):
 
 # ------ Training/Validation data loading -------
 
-path = './data/'
+path = './data_Udacity_2/'
 
 samples = get_driving_log(path)
 
@@ -108,23 +82,24 @@ train_generator = generator(path, train_samples, batch_size=32)
 validation_generator = generator(path, validation_samples, batch_size=32)
 
 row, col, ch =160, 320, 3  # Trimmed image format
-BATCH_SIZE = 32
 
 def resize_im(x):
     from keras.backend import tf
-    return tf.image.resize_images(x, (80, 160))
+    return tf.image.resize_images(x, (66, 160))
 
 model = Sequential()
-model.add(Cropping2D(cropping=((40,20), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,20), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 127.5 - 1))
 model.add(Lambda(resize_im))
 
 model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
 model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
 model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+
 model.add(Conv2D(64, 3, 3, activation='elu'))
+
 model.add(Conv2D(64, 3, 3, activation='elu'))
-model.add(Dropout(0.1))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100, activation='elu'))
 model.add(Dense(50, activation='elu'))
@@ -137,7 +112,7 @@ checkpoint = ModelCheckpoint('model-{epoch:03d}.h5', monitor='val_loss', verbose
 model.compile(loss='mse', optimizer='adam')
 
 
-model.fit_generator(train_generator, samples_per_epoch = 10000, validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5, callbacks=[checkpoint], verbose=1)
+model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5, callbacks=[checkpoint], verbose=1)
 model.save('model.h5')
 
 # Explicitly end tensorflow session
